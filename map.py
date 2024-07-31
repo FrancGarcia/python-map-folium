6f5fc28803433516fd7e52bdeeee76834e9c314e
Francisco Garcia <frgarcia@ucsd.edu>
2024-07-30 18:09:35 -0700

    Started the creation of the map using folium library

--- map.py
+++ map.py
@@ -1,41 +1,24 @@
 import folium
 
-lon, lat = 32.881830236836265, -117.23351828641518
-zoom_start = 25
+if __name__ == "__main__":
+    # create variables
+    map_filepath = "folium-map.html"
+    center_coord = [32.881978875195344, -117.23347001019796]
+    marker_coord = [32.881978875195344, -117.23347001019796]
+    marker_radius = 25_000
 
+    # create folium map
+    vmap = folium.Map(center_coord, zoom_start=9)
 
-m = folium.Map(location=[lon, lat], zoom_start=zoom_start)
-# Define the custom tile layer
-tiles = 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}'
-attr = ('&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver '
-        '(Contains Copernicus Data) | &copy; '
-        '<a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> '
-        '&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> '
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
+    # add a marker to the map
+    folium.vector_layers.Circle(
+        location=marker_coord,
+        tooltip=f"The marker has radius {marker_radius}",
+        radius=marker_radius,
+        color="red",
+        fill=True,
+        fill_color="red"
+    ).add_to(vmap)
 
-# Add the custom tile layer to the map
-folium.TileLayer(
-    tiles=tiles,
-    attr=attr,
-    name='Stadia Alidade Satellite',
-    min_zoom=0,
-    max_zoom=20,
-    ext='jpg'
-).add_to(m)
-
-folium.Marker(
-    location=[lon, lat],
-    tooltip="Click me!",
-    popup="CSE Basement",
-    icon=folium.Icon(icon="cloud"),
-).add_to(m)
-
-folium.Marker(
-    location=[32.88108881238462, -117.23740766672096],
-    tooltip="Click me!",
-    popup="Studying Center",
-    icon=folium.Icon(color="green"),
-).add_to(m)
-
-
-m.save("map.html")
+    # store the map to a file
+    vmap.save(map_filepath)
